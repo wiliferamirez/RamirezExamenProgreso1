e2591cf93f3edc4aaffbc25eefcf5c059f174038
WilliamRamirez <william.ramirez.paucar@udla.edu.ec>
2025-04-28 08:42:24 -0500

    Crear modelo para Plan Recompensa

---
+++ Models/PlanRecompensas.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RamirezExamenProgreso1.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace RamirezExamenProgreso1.Models
+{
+    public class PlanRecompensas
+    {
+        [Key]
+        public int RecompensasId { get; set; }
+
+        [Required(ErrorMessage = "Campo Obligatorio")]
+        public string NombreRecompensa { get; set; }
+
+        public int PuntosRecompensa { get; set; }
+
+        public string Categoria
+        {
+            get
+            {
+                if (PuntosRecompensa > 500)
+                {
+                    return "Gold";
+                }
+                else if (PuntosRecompensa < 500)
+                {
+                    return "Silver";
+                }
+                else
+                {
+                    return "Sin categoría";
+                }
+            }
+        }
+    }
+}
