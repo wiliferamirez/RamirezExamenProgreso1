64db93534f500a5a6da1d5a0a89fbd704df1618a
WilliamRamirez <william.ramirez.paucar@udla.edu.ec>
2025-04-28 08:30:17 -0500

    Crear modelo para reserva

---
+++ Models/Reserva.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RamirezExamenProgreso1.Models
+{
+    public class Reserva
+    {
+        [Key]
+        public int ReservaId { get; set; }
+        [ForeignKey("Clientes")]
+        public int ClienteId { get; set; }
+        public DateTime FechaRegistroCliente { get; set; }
+        public DateTime FechaSalidaCliente { get; set; }
+        [Required]
+        public int CantidadPersonas { get; set; }
+    }
+}
